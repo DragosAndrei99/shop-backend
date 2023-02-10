0fb137e4858f13f3c685b96bfbfc351bdf2d11af
Dragos Vasile <vdragos1999@yahoo.com>
2023-02-10 13:42:13 +0200

    Task 3.1: Add cors policy and change data variable naming in response object

--- Product-Service/src/functions/getProductsList/handler.ts
+++ Product-Service/src/functions/getProductsList/handler.ts
@@ -2,11 +2,12 @@ import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/api-gateway';
 import { formatJSONResponse } from '@libs/api-gateway';
 import { middyfy } from '@libs/lambda';
 import schema from './schema';
-
+import cors from '@middy/http-cors'
 import mockProductList from './productList';
 
 const getProductsList: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async () => {
-  return formatJSONResponse({mockProductList});
+  const data = mockProductList;
+  return formatJSONResponse({data});
 };
 
-export const main = middyfy(getProductsList);
+export const main = middyfy(getProductsList).use(cors());
