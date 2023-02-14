02ab199c8c35cb750b2a1cc129b7de16933ffb1c
Dragos Vasile <vdragos1999@yahoo.com>
2023-02-14 09:57:29 +0200

    Create getProductsById lambda

--- Product-Service/src/functions/getProductsById/handler.ts
+++ Product-Service/src/functions/getProductsById/handler.ts
@@ -5,9 +5,12 @@ import schema from './schema';
 import cors from '@middy/http-cors'
 import mockProductList from '../../../mock/mockDataProductList';
 
-const getProductsList: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async () => {
+const getProductsById: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
   const data = mockProductList;
-  return formatJSONResponse({data});
+  
+  const {productId} = event.pathParameters;
+  const foundProduct = data.filter(elem => elem.id === productId);
+  return formatJSONResponse({foundProduct});
 };
 
-export const main = middyfy(getProductsList).use(cors());
+export const main = middyfy(getProductsById).use(cors());
